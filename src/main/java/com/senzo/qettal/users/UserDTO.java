715f957f4d45ae2548df2046a7d2bc12b992b99d
leonardowolter <leocwolter@gmail.com>
2016-06-20 10:28:08 -0300

    Stores a sha256 version of the password

--- src/main/java/com/senzo/qettal/users/UserDTO.java
+++ src/main/java/com/senzo/qettal/users/UserDTO.java
@@ -1,5 +1,9 @@
 package com.senzo.qettal.users;
 
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -23,7 +27,13 @@ public class UserDTO {
 	}
 	
 	public User toModel() {
-		return new User(name, email, password);
+		try {
+			byte[] passwordBytes = password.getBytes("UTF-8");
+			byte[] hashedPassword = MessageDigest.getInstance("SHA-256").digest(passwordBytes);
+			return new User(name, email, new String(hashedPassword));
+		} catch (NoSuchAlgorithmException | UnsupportedEncodingException e) {
+			throw new RuntimeException("Coulndn't hash the password of the user " + email, e);
+		}
 	}
 
 }
