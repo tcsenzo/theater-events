0dde0a1a268331743bed6f8046dc4abdea84334e
leonardowolter <leocwolter@gmail.com>
2016-09-10 16:31:35 -0300

    Adds hikaricp

---
+++ src/main/java/com/senzo/qettal/theaterEvents/persistence/DataSourceConfig.java
@@ -0,0 +1,23 @@
+package com.senzo.qettal.theaterEvents.persistence;
+
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+@Configuration
+@ConfigurationProperties(prefix = "qettal.datasource")
+public class DataSourceConfig extends HikariConfig {
+
+    @Bean
+    public DataSource dataSource() throws SQLException {
+        return new HikariDataSource(this);
+    }
+
+}
